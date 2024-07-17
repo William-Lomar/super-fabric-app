dddc7dfd407ac397f88021d4319594ea8eba28a1
William Lomar <lomarw99@gmail.com>
2024-07-16 21:37:12 -0300

    add mui lib to interfaces components, create structure to add editors

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { SuperFabric } from 'super-fabric';
+import { Accordion, AccordionDetails, AccordionSummary, Button, Drawer, Typography } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import EditDimensionsComponent from './components/editDimension';
+import CloseIcon from '@mui/icons-material/Close';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const superfabric = useRef<SuperFabric | undefined>(undefined);
+
+  useEffect(() => {
+    SuperFabric.initialize('superfabric').then((superf) => {     
+      superfabric.current = superf; 
+    }).catch((err) => {
+      console.error('error on initialize SuperFabric: ', err);
+    })
+  }, []);
+
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  const editors: { header: string, component: JSX.Element }[] = [
+    { header: 'Dimensions', component: <EditDimensionsComponent superfabric={ superfabric.current }></EditDimensionsComponent> }
+  ];
+
+  const content =
+    <div style={{ width: '100%', height: '100%' }}>
+      <div className='button'>
+        <Button onClick={() => setShowDrawer(true)} variant="outlined">
+          Open editors
+        </Button>
+      </div>
+
+      <div className='container'>
+        <div id='superfabric' className='container-superfabric'></div>
+      </div>
+
+      <Drawer open={showDrawer} variant="persistent" anchor='right'>
+        <div className='button'>
+          <Button onClick={() => setShowDrawer(false)} variant="outlined" endIcon={< CloseIcon />}>
+            Close
+          </Button>
+        </div>
+        <div style={{ width: '400px' }}>
+          {
+            editors.map((editor, index) => {
+              const content =
+                <Accordion key={'accordion' + index}>
+                  <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel2-content"
+                    id={'editor' + index}
+                  >
+                    <Typography>{editor.header}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    {editor.component}
+                  </AccordionDetails>
+                </Accordion>
+
+              return content
+            })
+          }
+        </div>
+      </Drawer>
     </div>
-  );
-}
 
-export default App;
+  return content;
+}
